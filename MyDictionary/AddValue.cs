731e33615b3dad4f4b8551eb973e1923b4e6ca90
Emre Koçyatağı <londonjack731@gmail.com>
2022-09-16 13:54:21 +0300

    Add project files.

---
+++ MyDictionary/AddValue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class AddValue<TK, TV>
+    {
+        public TK[] Key;
+        public TV[] Value;
+        public TK[] TempKey;
+        public TV[] TempValue;
+        public AddValue()
+        {
+            Key = new TK[0];
+            Value = new TV[0];
+        }
+        public void Add(TK key, TV value)
+        {
+            TempKey = Key;
+            TempValue = Value;
+            Key = new TK[Key.Length + 1];
+            Value = new TV[Value.Length + 1];
+            for (int i = 0; i < TempKey.Length; i++)
+            {
+                Key[i] = TempKey[i];
+                Value[i] = TempValue[i];
+
+            }
+            Key[Key.Length - 1] = key;
+            Value[Value.Length - 1] = value;
+        }
+        public void List()
+        {
+            for (int i = 0; i < TempKey.Length; i++)
+            {
+                Console.WriteLine($"{Key[i]} {Value[i]}");
+
+
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+
+
+//Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+
+AddValue<int, string> keyValuePairs = new AddValue<int, string>();
+
+keyValuePairs.Add(1, "JANUARY");
+keyValuePairs.Add(2, "FEBRUARY");
+keyValuePairs.Add(3, "MARCH");
+keyValuePairs.Add(4, "APRIL");
+//list fonksiyonu ile degerlerin eklendigini gorebilirsin.
+keyValuePairs.List();
